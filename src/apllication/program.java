e1d340f83c17c0416299b83db07a2258619db714
Naor Santos <naorsantos@gmail.com>
2020-07-27 19:04:43 -0300

    Solução ruin

--- src/apllication/program.java
+++ src/apllication/program.java
@@ -11,45 +11,42 @@ import model.entities.Reservation;
 public class program {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Numero do Quarto:");
 		int number = sc.nextInt();
 		System.out.print("Check- in date  (dd/MM/yyyy):");
 		Date checkIn = sdf.parse(sc.next());
 		System.out.print("Check- out date  (dd/MM/yyyy):");
 		Date checkOut = sdf.parse(sc.next());
-		
-		if(!checkOut.after(checkIn)) {
+
+		if (!checkOut.after(checkIn)) {
 			System.out.println("Erro na reserva: Check-out deve ser depois da data de Check-in");
-		}
-		else {
-			Reservation reservation =  new Reservation(number, checkIn, checkOut);
+		} else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation:" + reservation);
-			
+
 			System.out.println();
 			System.out.println("Entre com a data para atualizar a reserva:");
 			System.out.print("Check- in date  (dd/MM/yyyy):");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check- out date  (dd/MM/yyyy):");
 			checkOut = sdf.parse(sc.next());
-			
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Erro nas reserva: Resevars so podem ser atualizadas para datas futuras.");
+
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+			System.out.println("Erro na reserva:" + error);
 			}
-			else if(!checkOut.after(checkIn)) {
-				System.out.println("Erro na reserva: Check-out deve ser depois da data de Check-in");
-			}
-			else {
-			reservation.updateDates(checkIn, checkOut);
+			else {	
 			System.out.println("Reservation:" + reservation);
+				
+				}
+			}
+			sc.close();
+
 		}
-		sc.close();
 
 	}
 
-}
-}
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -34,9 +34,18 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "Resevars so podem ser atualizadas para datas futuras.";
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Erro na reserva: Check-out deve ser depois da data de Check-in";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
